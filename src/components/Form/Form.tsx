0e8732f5a2dc79c865750ddfbe36df8fd2667ad5
dilipv <dilipvishwakarma7878@gmail.com>
2024-08-23 15:43:57 +0530

    final changes

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -7,7 +7,7 @@ export const Form = () => {
     <div className="py-10 bg-[#F8F8F8]">
       <Heading className="text-center">Send A Message</Heading>
       <div className="mt-8">
-        <div className="md:w-6/12 m-auto md:p-10 p-5 bg-white rounded-xl shadow-[rgba(0,0,0,0.16)_0px_1px_4px]">
+        <div className="md:w-6/12 m-auto p-8 bg-white rounded-xl shadow-[rgba(0,0,0,0.16)_0px_1px_4px]">
           <ContactForm />
         </div>
       </div>
